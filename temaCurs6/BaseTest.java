871700b918bb09e5c13491f27db0c6a55da77627
MoldovanAndrada <50479889+MoldovanAndrada@users.noreply.github.com>
2022-01-05 21:34:36 +0200

    Add files via upload

---
+++ temaCurs6/BaseTest.java
@@ -0,0 +1,52 @@
+package utils;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import com.beust.jcommander.Parameter;
+
+
+import pages.NavMenuPage;
+
+public class BaseTest {
+	
+	
+
+	public WebDriver driver;
+	public NavMenuPage navMenu;
+	
+	@Parameters({"url"})
+	@BeforeClass
+	public void setUp(String url) {		
+		//System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver =  new ChromeDriver();	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+		//driver.get("https://keybooks.ro/");
+		driver.get(url);
+		
+		navMenu = new NavMenuPage(driver);
+
+	}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(4000);
+		driver.quit();
+	}
+	
+	public void navigateToAccount() {
+		driver.get("https://keybooks.ro/account/");
+	}
+	
+	public void navigateToShop () {
+		driver.get("https://keybooks.ro/shop/");
+	}
+	
+
+}
